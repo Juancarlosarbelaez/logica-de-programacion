6977eee2f433bdbb42abff849dad866db6b85990
Juancarlosarbelaez <71791695+Juancarlosarbelaez@users.noreply.github.com>
2020-09-23 15:29:02 -0500

    Add files via upload

---
+++ bandera.java
@@ -0,0 +1,45 @@
+public class bandera{
+	public static void main(String[] args) {
+		
+		char matriz[][];//Declaración
+		matriz = new char[70][70];
+
+		for (int i=0; i<matriz.length; i++ )
+		{
+			for (int j=0; j<matriz.length ; j++ )
+			{
+				if(i<j)
+				{
+                   matriz[i][j]= 'r';
+				}
+
+				else
+				{
+                  matriz[i][j]= 'n';
+				}
+
+			}
+		}
+
+
+		for (int i=0; i<matriz.length; i++ )
+		{
+			for (int j=0; j<matriz.length ; j++ )
+			{
+				if(matriz[i][j]=='r')
+				{
+					System.out.print(ConsoleColors.RED_BACKGROUND + "  ");
+				}
+
+				else if (matriz[i][j]=='n') 
+				{
+				   System.out.print(ConsoleColors.BLACK_BACKGROUND + "  ");	
+				}
+
+				System.out.print(ConsoleColors.RESET);
+			}
+
+			System.out.println("");
+		}    	 
+	}   
+}
